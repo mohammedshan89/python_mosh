d5b63db648cd7e6c55816b650ad103867fcc68bf
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-17 11:06:32 +0530

    if statement

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+house_price = 100000
+credit_score = True
+if credit_score :
+  down_payment = 0.1*house_price
+else :
+  down_payment = 0.2*house_price
+print(f"Down payment: {round(down_payment)}")
