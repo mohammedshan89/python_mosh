d80f11bab7cbffa1c047dcd5e3d62b31c90e1bb3
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-17 11:10:24 +0530

    logical operation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 
-house_price = 100000
-credit_score = True
-if credit_score :
-  down_payment = 0.1*house_price
-else :
-  down_payment = 0.2*house_price
-print(f"Down payment: {round(down_payment)}")
+# house_price = 100000
+# credit_score = True
+# if credit_score :
+#   down_payment = 0.1*house_price
+# else :
+#   down_payment = 0.2*house_price
+# print(f"Down payment: {round(down_payment)}")
+
+is_good_credit = False
+has_high_income = True
+
+if has_high_income or is_good_credit:
+  print("Eligible for loan")
