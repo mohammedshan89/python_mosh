2354d5670a8ef5d5dbe8a59c9d8340d7d04deca5
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-17 11:32:42 +0530

    python small assinment weight converter for understanding if statement and how to take input from terminal

--- main.py
+++ main.py
@@ -8,8 +8,32 @@
 #   down_payment = 0.2*house_price
 # print(f"Down payment: {round(down_payment)}")
 
-is_good_credit = False
-has_high_income = True
+# is_good_credit = False
+# has_high_income = True
+# is_criminal_record = False
+# if has_high_income and not is_good_credit:
+#   print("Eligible for loan")
 
-if has_high_income or is_good_credit:
-  print("Eligible for loan")
+# logical operation
+# and or and not
+
+# comparison operator
+
+# temperature = 30
+
+# if temperature == 30:
+#   print("Its hot day")
+# else:
+#   print("its not hot day")
+
+# project weight converter
+
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+
+if unit.upper() == 'L':
+  converted = weight * 0.45
+  print(f"You are {converted} kilos")
+elif unit.upper() == "K":
+  converted = weight / 0.45
+  print(f"you are {converted} lbs")
